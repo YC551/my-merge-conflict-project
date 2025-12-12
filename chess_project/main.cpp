e4878ae2b6f6b4497dd9dbbe313226a0f5eb94cd
ShovAm07 <amitshoval07@gmail.com>
2025-12-12 18:02:20 +0200

    fit : added checks to main

--- chess_project/main.cpp
+++ chess_project/main.cpp
@@ -7,43 +7,44 @@
 int main()
 {
     Board board;
-    MoveValidator validator;
+
 
     // white pices
-    board.pieces[7][0] = new Rook(7, 0, false);  // a1 - white rook
-    board.pieces[7][7] = new Rook(7, 7, false);  // h1 - white rook
-    board.pieces[7][3] = new King(7, 3, false);  // d1 - white king
+    board.pieces[0][0] = new Rook(0, 0, false);  // a8 - white rook
+    board.pieces[0][7] = new Rook(0, 7, false);  // h8 - white rook
+    board.pieces[0][3] = new King(0, 3, false);  // d8 - white king
 
 
 
     // black pices
-    board.pieces[0][0] = new Rook(0, 0, true);   // a8 - black rook
-    board.pieces[0][7] = new Rook(0, 7, true);   // h8 - black rook
-    board.pieces[0][3] = new King(0, 3, true);   // d8 - black king
+    board.pieces[7][0] = new Rook(7, 0, true);   // a1 - black rook
+    board.pieces[7][7] = new Rook(7, 7, true);   // h1 - black rook
+    board.pieces[7][3] = new King(7, 3, true);   // d1 - black king
 
-    
     std::cout << "Initial board setup:\n";
-    board.printBoard();
-
-    board.changePieceLocation("a1d2");
     
 
-    
 
-    
-    board.printBoard();
-
-    if (validator.isCheck(&board, true))//check if there is a check on black
-    {
-        std::cout << "black check\n";
-    }
-
-
-    if (validator.isCheck(&board, false))//check if there is a check on white
-    {
-        std::cout << "white check\n";
-    }
    
+    board.changePieceLocation("a8b1");
+    //board.changePieceLocation("h8d6");
+    board.printBoard();
 
+    std::string updateStr = "a1b1";
+    board.changePieceLocation("a1b1");
+    board.changePieceLocation("b1a1");
+    board.printBoard();
+    std::string srcLocation = updateStr.substr(0, 2); //get first part
+    std::string dstLocation = updateStr.substr(2, 2); //get second part
+
+    //we get the src and destination for the array
+    int srcRow = board.getRowFromString(srcLocation);
+    int srcCol = board.getColFromString(srcLocation);
+
+    int dstRow = board.getRowFromString(dstLocation);
+    int dstCol = board.getColFromString(dstLocation);
+    
+    std::cout << "code" << MoveValidator::validateMove(&board, srcRow, srcCol, dstRow, dstCol, true);
+  
     return 0;
 }
